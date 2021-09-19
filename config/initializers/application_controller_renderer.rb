1039dccf3ffcf9d214528e9a878c758604fa907b
amaki <in345vain@gmail.com>
2021-09-19 09:18:10 +0900

    second commit

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2a9d7213c1916cb6f3e4902de80784654c4160d23aab45533a4c9a94662058c12fb07a082726b15875bf2f9e9acf82ae2202bf8ac2f1bdabe1b3ab7d34cca30a'
